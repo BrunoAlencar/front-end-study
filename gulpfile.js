63d8fbd1750b7cf23ea8d579da4b4929cae49ce5
Bruno Alencar <joaobrunoalencar@gmail.com>
2018-03-07 18:22:47 +0000

    gulp browser sync script

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync');
 
 
 gulp.task('sass', () => {
@@ -10,4 +11,17 @@ gulp.task('sass', () => {
 
 gulp.task('listen', () => {
     gulp.watch('./src/sass/**/*.scss', ['sass'])
+})
+
+
+gulp.task('server', () => {
+    browserSync.init({
+        server: {
+            baseDir: 'src'
+        }
+    });
+
+    gulp.watch('./src/**/*').on('change', browserSync.reload);
+
+    gulp.watch('./src/sass/**/*.scss', ['sass']);
 })
